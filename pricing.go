099c8c30302e6e90503284ee153876dcd26e6f00
nepdave <dave@perka.com>
2017-12-03 14:06:45 -0500

    got bidAsk function working

--- pricing.go
+++ pricing.go
@@ -1,1 +1,18 @@
-//package fxtechnical
+package main
+
+import (
+	"fmt"
+	"github.com/nepdave/oanda"
+)
+
+func bidAsk(instrument string) (string, string) {
+	pricing := oanda.Pricing{}.UnmarshalPricing(oanda.GetPricing(instrument))
+
+	return pricing.Prices[0].Bids[0].Price, pricing.Prices[0].Asks[0].Price
+}
+
+func main() {
+	blah, b := bidAsk("EUR_USD")
+	fmt.Println(blah)
+	fmt.Println(b)
+}
