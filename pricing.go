c13d33833d1b7b56c475dcb9999f86cfb13f13fe
nepdave <dave@perka.com>
2017-12-05 18:14:09 -0500

    cleaned up CloseAverage and now passing command line args for count

--- pricing.go
+++ pricing.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-  "strconv"
-  "log"
 	"github.com/nepdave/oanda"
+	"log"
+	"os"
+	"strconv"
 )
 
 func BidAsk(instrument string) (string, string) {
@@ -15,55 +16,36 @@ func BidAsk(instrument string) (string, string) {
 
 func Candles(instrument string, count string, granularity string) *oanda.Candles {
 	return oanda.Candles{}.UnmarshalCandles(oanda.GetCandles(instrument, count,
-    granularity))
+		granularity))
 }
 
 //FIXME this should have a unit test!
 func CloseAverage(candles *oanda.Candles) float64 {
-  sum := 0.0
-  counter := 0
-  fmt.Println("LEN CANDLES:")
-  fmt.Println(len(candles.Candles))
-
-  for _, v := range candles.Candles {
-    //FIXME doing a type assertion here. this is new to me...
-    if str, ok := v.Mid["c"].(string); ok {
-        /* act on str */
-        f, err := strconv.ParseFloat(str, 64)
-        //FIXME need to work on error handling
-        if err != nil {
-            log.Fatal(err)
-        }
-        sum = sum + f
-    } else {
-        /* not string */
-        log.Fatal("CloseAverage type assertion error")
-    }
-    counter++
-
-
-  }
-  fmt.Println("COUNTER:")
-  fmt.Println(counter)
-  return sum/float64(len(candles.Candles))
+	sum := 0.0
+	for _, v := range candles.Candles {
+		//FIXME doing a type assertion here. this is new to me...
+		if str, ok := v.Mid["c"].(string); ok {
+			/* act on str */
+			f, err := strconv.ParseFloat(str, 64)
+			//FIXME need to work on error handling
+			if err != nil {
+				log.Fatal(err)
+			}
+			sum = sum + f
+		} else {
+			/* not string */
+			log.Fatal("CloseAverage type assertion error")
+		}
+	}
+	return sum / float64(len(candles.Candles))
 }
 
-
-
 func main() {
-	//blah, b := bidAsk("EUR_USD")
-	//fmt.Println(blah)
-	fmt.Println("start")
-	//oanda.GetCandles("EUR_USD", "1", "D")
-	//close := close("EUR_USD", "2", "D")
-  //fmt.Println(close)
-	//fmt.Println(close.Candles[0].Mid["c"])
-
-  candles := Candles("EUR_USD", "10", "D")
-  closeAverage := CloseAverage(candles)
-  fmt.Println("**********")
-  fmt.Println("AVERAGE:")
-  fmt.Println(closeAverage)
-  fmt.Println("**********")
-	fmt.Println("end")
+	count := os.Args
+	candles := Candles("EUR_USD", count[1], "D")
+	closeAverage := CloseAverage(candles)
+	fmt.Println("**********")
+	fmt.Println("AVERAGE:")
+	fmt.Printf("%6.6f\n", closeAverage)
+	fmt.Println("**********")
 }
