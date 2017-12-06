2b12ba331238a57907c5ad59036a02af05b8e43e
nepdave <dave@perka.com>
2017-12-06 11:51:46 -0500

    added count parameter to closeAverages

--- pricing.go
+++ pricing.go
@@ -20,8 +20,13 @@ func Candles(instrument string, count string, granularity string) *oanda.Candles
 }
 
 //FIXME this should have a unit test!
-func CloseAverage(candles *oanda.Candles) float64 {
+func CloseAverage(candles *oanda.Candles, count string) float64 {
 	sum := 0.0
+	i, err := strconv.Atoi(count)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	for _, v := range candles.Candles {
 		//FIXME doing a type assertion here. this is new to me...
 		if str, ok := v.Mid["c"].(string); ok {
@@ -37,13 +42,13 @@ func CloseAverage(candles *oanda.Candles) float64 {
 			log.Fatal("CloseAverage type assertion error")
 		}
 	}
-	return sum / float64(len(candles.Candles))
+	return sum / float64(i)
 }
 
 func main() {
 	count := os.Args
 	candles := Candles("EUR_USD", count[1], "D")
-	closeAverage := CloseAverage(candles)
+	closeAverage := CloseAverage(candles, count[1])
 	fmt.Println("**********")
 	fmt.Println("AVERAGE:")
 	fmt.Printf("%6.6f\n", closeAverage)
