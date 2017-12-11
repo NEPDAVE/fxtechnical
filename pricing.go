e1d7ae64427b31c7fdb6122c83946fb39b664f4a
nepdave <dave@perka.com>
2017-12-10 19:26:35 -0500

    added up trend func too much printhing though

--- pricing.go
+++ pricing.go
@@ -40,15 +40,41 @@ func CloseAverage(candles *oanda.Candles, count string) float64 {
 	return sum / float64(i)
 }
 
+func trend(instrument string, count string, granularity string) string {
+	candles := Candles(instrument, count, "D")
+	closeAverage := CloseAverage(candles, count)
+	bid, _ := BidAsk(instrument)
+
+	fBid, err := strconv.ParseFloat(bid, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if fBid > closeAverage {
+		return "up"
+	}else if fBid < closeAverage {
+		return "down"
+	}else if fBid == closeAverage {
+		return "equal"
+	}else {
+		return "wtf"
+	}
+
+}
+
 func main() {
 	count := os.Args
 	candles := Candles("EUR_USD", count[1], "D")
 	bid, _ := BidAsk("EUR_USD")
 	closeAverage := CloseAverage(candles, count[1])
+	way := trend("EUR_USD", count[1], "D")
 	fmt.Println("**********")
 	fmt.Println("AVERAGE:")
 	fmt.Printf("%6.6f\n", closeAverage)
 	fmt.Println("BID:")
 	fmt.Println(bid)
+	fmt.Println("Trend:")
+	//FIXME calling this prints out a whole bunch of shit...
+  fmt.Println(way)
 	fmt.Println("**********")
 }
