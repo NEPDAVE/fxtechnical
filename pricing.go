73b364d225b2b4817253838be372030378789780
nepdave <dave@perka.com>
2017-12-13 21:00:02 -0500

    trying to add bidaskmultiple function so cc index does not make repeated api calls

--- pricing.go
+++ pricing.go
@@ -8,6 +8,38 @@ import (
 	"strconv"
 )
 
+//FIXME you can add a second function that can mostly using the existing
+//oanda endpoint function and a second bidask(multiple) or maybe you could one
+//becasue your getting back a list of prices and we do not want to assume the
+//prices are going to be in order, we will want to loop through the struct to pull
+//out the prices, then we can use them for things like the index page or doing
+//statistical arbitrage, so that's cool...
+
+//TODO write a BidAsk multiple function and a GetPricing function that can
+//handle the variadic input
+
+//TODO figure out if you need to do anykind of analysis for the multiple prices
+//that are returned.... that is annoying
+
+
+//We need a list of all the instruments to reference them later...
+func BidAskMultiple(intruments ...string) map[string]string {
+	pricing := oanda.Pricing{}.UnmarshalPricing(oanda.GetMultiplePricing(instruments))
+
+  instrumentsMap := map[string]string
+
+	for i, v range := pricing {
+		instrumentsMap[]
+		instrument := pricing.Prices[i].Instrument
+		price := pricing.Prices[i].Asks[0].Price
+		//FIXME double check this is right way to add k/v for go
+		instrumentsMap[instrument] = price
+	}
+
+	return instrumentsMap
+
+}
+
 func BidAsk(instrument string) (string, string) {
 	pricing := oanda.Pricing{}.UnmarshalPricing(oanda.GetPricing(instrument))
 
@@ -66,6 +98,9 @@ func main() {
 	count := os.Args
 	candles := Candles("EUR_USD", count[1], "D")
 	bid, _ := BidAsk("EUR_USD")
+	fmt.Println("**********")
+	fmt.Println("BREAK:BREAK:BREAK:BREAK:BREAK:BREAK:BREAK:BREAK:BREAK:")
+	fmt.Println("**********")
 	closeAverage := CloseAverage(candles, count[1])
 	way := trend("EUR_USD", count[1], "D")
 	fmt.Println("**********")
