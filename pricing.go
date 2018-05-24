d83f200adf746a30d2a9dcb6dae1586ed6608a2e
nepdave <dave@perka.com>
2018-05-24 11:02:13 -0700

    added streambidask function

--- pricing.go
+++ pricing.go
@@ -1,10 +1,33 @@
 package fxtechnical
 
 import (
-	//"log"
+	"fmt"
 	"strconv"
 	oanda "github.com/nepdave/oanda"
 )
+
+func StreamBidAsk(instruments string, out chan []byte) {
+	oandaChan := make(chan []byte)
+ 	go oanda.StreamPricing("EUR_USD", oandaChan)
+  for priceByte := range oandaChan {
+		//appears that pricesBytes are 266 long
+		if len(priceByte) == 266 {
+			prices := oanda.Prices{}.UnmarshalPrices(priceByte)
+			//return pricing.Prices[0].Bids[0].Price, pricing.Prices[0].Asks[0].Price
+			fmt.Println("Bid:")
+			fmt.Println(prices.Bids[0].Price)
+			fmt.Println("Ask:")
+			fmt.Println(prices.Asks[0].Price)
+			fmt.Println("BYTE:")
+			//fmt.Println(string(priceByte))
+			//fmt.Println(pricing)
+			//appears that heartbeats are 61 long
+		} else if len(priceByte) == 61 {
+			 heartbeat := oanda.Heartbeat{}.UnmarshalHeartbeat(priceByte)
+		  fmt.Println(heartbeat.Time)
+		}
+	}
+}
 
 //FIXME think about having this funct return float64 instead of string so you
 //can immediatly do math with the return values
@@ -13,8 +36,9 @@ func BidAsk(instrument string) (string, string) {
 
 	if err != nil {
 		//FIXME think this through... if the caller is trying to do a type
-		//conversion then this will err out
-		return "prices unavailable", "prices unavailable"
+		//conversion then this will err out if you return a word
+		//one possibility...
+		return "0","0"
 	}
 	pricing := oanda.Pricing{}.UnmarshalPricing(pricingByte)
 	return pricing.Prices[0].Bids[0].Price, pricing.Prices[0].Asks[0].Price
