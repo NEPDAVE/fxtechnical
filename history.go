267601d40fce64fb34d8563b330637a043be244c
nepdave <nepdave1@gmail.com>
2018-07-18 14:05:41 -0700

    added better error handling to candles func

--- history.go
+++ history.go
@@ -1,19 +1,15 @@
 package fxtechnical
 
 import (
-	//"log"
-	//"strconv"
-	"errors"
 	oanda "github.com/nepdave/oanda"
 )
 
+//Candles returns a *oanda.Candles, used by CloseAverage
 func Candles(instrument string, count string, granularity string) (*oanda.Candles, error) {
 	candlesByte, err := oanda.GetCandles(instrument, count, granularity)
 
 	if err != nil {
-		//FIXME not really sure what to do here becuase of the type this func
-		//is expecting to return
-		return oanda.Candles{}.UnmarshalCandles([]byte{}), errors.New("Candles Error")
+		return &oanda.Candles{}, err
 	}
-	return oanda.Candles{}.UnmarshalCandles(candlesByte), errors.New("Candles Error")
+	return oanda.Candles{}.UnmarshalCandles(candlesByte), err
 }
