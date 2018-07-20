8abfa7691e398a94e930775e09ca00d091dfe815
nepdave <nepdave1@gmail.com>
2018-07-20 11:31:19 -0700

    coded out ContinuosRaid with channel support

--- strategies.go
+++ strategies.go
@@ -27,7 +27,7 @@ func (r Raider) SingleRaid(bb BollingerBand, units int) Raider {
 	}
 
 	//calling CalculateSpread also sets bid/ask fields in struct
-	pricesData.CalculateSpread()
+	//pricesData.CalculateSpread()
 
 	//FIXME need to have better error handling here
 	if pricesData.Bid > bb.UpperBand {
@@ -48,34 +48,54 @@ func (r Raider) SingleRaid(bb BollingerBand, units int) Raider {
 		}
 	} else {
 		return Raider{
+			Orders: oanda.Orders{}.MarketBuyOrder(pricesData.Bid,
+				pricesData.Ask,
+				bb.Instrument,
+				units),
 			ExecuteOrder: 0,
 		}
 	}
 
 }
 
-// //ContinuousRaid ranges over a channel of PricesData comparing each PricesData to a
-// //BollingerBand and sends a trading decision over a channel to the caller
-// func (r Raider) ContinuousRaid(bb BollingerBand, units int) {
-// 	//initializing pricesData struct
-// 	pricesData := PricesData{}.Init(bb.Instrument)
-//
-// 	if pricesData.Error != nil {
-// 		//log.Println(pricesData.Error)
-// 	}
-//
-// 	//calling CalculateSpread also sets bid/ask fields in struct
-// 	pricesData.CalculateSpread()
-//
-// 	if pricesData.Bid > bb.UpperBand {
-// 		//returning Orders struct along with a 1. 1 = submit order
-// 		oanda.Orders{}.MarketSellOrder(bid, ask, instrument, units), 1
-// 	} else if pricesData.Ask < bb.LowerBand {
-// 		//returning Orders struct along with a 1. 1 = submit order
-// 		return oanda.Orders{}.MarketBuyOrder(bid, ask, instrument, units), 1
-// 	} else {
-// 		//returning empty Orders struct along with a 0. 0 = don't submit order
-// 		return oanda.Orders{}, 0
-// 	}
-//
-// }
+//ContinuousRaid ranges over a channel of PricesData comparing each PricesData to a
+//BollingerBand and sends a trading decision over a channel to the caller
+//FIXME if running this function coninuosly be careful to generate a new
+//bollinger band at the start of each day
+func (r Raider) ContinuousRaid(bb BollingerBand, units int, out chan Raider) {
+	oandaChan := make(chan PricesData)
+	go StreamBidAsk(bb.Instrument, oandaChan)
+
+	for pricesData := range oandaChan {
+		if pricesData.Error != nil {
+			out <- Raider{Error: pricesData.Error}
+		}
+
+		//FIXME need to have better error handling here
+		if pricesData.Bid > bb.UpperBand {
+			out <- Raider{
+				Orders: oanda.Orders{}.MarketSellOrder(pricesData.Bid,
+					pricesData.Ask,
+					bb.Instrument,
+					units),
+				ExecuteOrder: 1,
+			}
+		} else if pricesData.Ask < bb.LowerBand {
+			out <- Raider{
+				Orders: oanda.Orders{}.MarketBuyOrder(pricesData.Bid,
+					pricesData.Ask,
+					bb.Instrument,
+					units),
+				ExecuteOrder: 1,
+			}
+		} else {
+			out <- Raider{
+				Orders: oanda.Orders{}.MarketBuyOrder(pricesData.Bid,
+					pricesData.Ask,
+					bb.Instrument,
+					units),
+				ExecuteOrder: 0,
+			}
+		}
+	}
+}
