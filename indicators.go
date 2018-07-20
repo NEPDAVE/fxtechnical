957b69677240dac91b83ed5f2a166c5957ce7f3e
nepdave <nepdave1@gmail.com>
2018-07-20 10:17:08 -0700

    fixed variable name errors in bb and dbb

--- indicators.go
+++ indicators.go
@@ -8,34 +8,54 @@ BollingerBand structs and methods
 
 //BollingerBand contains unmarshaled prices data and methods to populate struct fields
 type BollingerBand struct {
-	UpperBand float64
-	Average float64
-	lowerBand float64
-	Error error
+	UpperBand   float64
+	Average     float64
+	LowerBand   float64
+	Instrument  string
+	Count       string
+	Granularity string
+	Error       error
 }
 
 //Init populates and returns BollingerBand struct
-func (b BolingerBand) Init(instrument string, count int, granularity string) BollingerBand {
-	candles, _ := Candles("instrument", count, granularity)
-	b.Average, pricesSlice = CloseAverage(candles, count)
-	sd := fxtech.StandardDeviation(average, pricesSlice)
+//FIXME currently not doing any error checking here
+func (b BollingerBand) Init(instrument string, count string, granularity string) BollingerBand {
+	candles, _ := Candles(instrument, count, granularity)
+	average, pricesSlice := CloseAverage(candles, count)
+	b.Average = average
+	sd := StandardDeviation(average, pricesSlice)
 	b.UpperBand = average + (sd * 2)
 	b.LowerBand = average - (sd * 2)
+	b.Instrument = instrument
+	b.Count = count
+	b.Granularity = granularity
+
+	return b
 }
 
 //DoubleBollingerBand contains unmarshaled prices data and methods to populate struct fields
 type DoubleBollingerBand struct {
-	UpperBand float64
-	Average float64
-	lowerBand float64
-	Error error
+	UpperBand   float64
+	Average     float64
+	LowerBand   float64
+	Instrument  string
+	Count       string
+	Granularity string
+	Error       error
 }
 
 //Init populates and returns DoubleBollingerBand struct
-func (d DoubleBolingerBand) Init(instrument string, count int, granularity string) DoubleBollingerBand {
-	candles, _ := Candles("instrument", count, granularity)
-	d.Average, pricesSlice = CloseAverage(candles, count)
-	sd := fxtech.StandardDeviation(average, pricesSlice)
+//FIXME currently not doing any error checking here
+func (d DoubleBollingerBand) Init(instrument string, count string, granularity string) DoubleBollingerBand {
+	candles, _ := Candles(instrument, count, granularity)
+	average, pricesSlice := CloseAverage(candles, count)
+	d.Average = average
+	sd := StandardDeviation(average, pricesSlice)
 	d.UpperBand = average + (sd * 3)
 	d.LowerBand = average - (sd * 3)
+	d.Instrument = instrument
+	d.Count = count
+	d.Granularity = granularity
+
+	return d
 }
