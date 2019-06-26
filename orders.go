5d6b62d1407294066ea35739032f58ebd8971308
David Streckert <david@Davids-MBP.domain>
2019-06-25 20:38:57 -0700

    added support for simple orders. IE no stoploss

--- orders.go
+++ orders.go
@@ -16,14 +16,73 @@ func FxTechInit(logger *log.Logger) {
 
 /*
 ***************************
-Collection of functions to create orders with oanda
+Functions to create and prepare Simple Orders
+***************************
+*/
+
+//CreateOrder creates the order data structure, submits the order to Oanda,
+//and returns an OrderID
+func CreateSimpleOrder(units string, instrument string, takeProfit string) (*oanda.OrderCreateTransactionResponse, error) {
+
+	//building market order struct
+	orders := SimpleMarketOrders(units, instrument, takeProfit)
+
+	//marshaling market order struct into byte slice
+	ordersByte := oanda.SimpleOrdersRequest{}.MarshalSimpleOrdersRequest(orders)
+
+	fmt.Println(string(ordersByte))
+
+	//posting market order byte slice to oanda api
+	response, err := oanda.CreateOrder(ordersByte)
+
+	fmt.Println(string(response))
+
+	if err != nil {
+		logger.Println(err)
+		return &oanda.OrderCreateTransactionResponse{}, err
+	}
+
+	//unmarshaling order create transaction into struct
+	transaction, err := oanda.OrderCreateTransactionResponse{}.
+		UnmarshalOrderCreateTransactionResponse(response)
+
+	return transaction, err
+}
+
+//MarketOrder builds struct needed for marshaling data into a []byte
+func SimpleMarketOrders(units string, instrument string, takeProfitPrice string) oanda.SimpleOrdersRequest {
+
+	//FIXME need to figure out how to create trailing stop loss
+	//trailingStopLossOnFill := oanda.TrailingStopLossOnFill{}
+
+	//take profit data
+	takeProfitOnFill := oanda.TakeProfitOnFill{
+		TimeInForce: "GTC", Price: takeProfitPrice,
+	}
+
+	//order data
+	orders := oanda.SimpleOrdersRequest{
+		Orders: oanda.SimpleOrders{
+			TakeProfitOnFill: takeProfitOnFill,
+			TimeInForce:      "FOK",
+			Instrument:       instrument,
+			Units:            units,
+			Type:             "MARKET",
+			PositionFill:     "DEFAULT"},
+	}
+
+	return orders
+}
+
+/*
+***************************
+Functions to create Normal Orders
 ***************************
 */
 
 //CreateOrder creates the order data structure, submits the order to Oanda,
 //and returns an OrderID
 func CreateOrder(units string, instrument string, stopLoss string, takeProfit string) (*oanda.OrderCreateTransactionResponse, error) {
-
 
 	//building market order struct
 	orders := MarketOrder(units, instrument, stopLoss, takeProfit)
@@ -53,15 +112,6 @@ func CreateOrder(units string, instrument string, stopLoss string, takeProfit st
 	return transaction, err
 }
 
-/*
-***************************
-Collection of functions to prepare orders
-***************************
-*/
-
-//SimpleMarketOrder
-
-
 //MarketOrder builds struct needed for marshaling data into a []byte
 func MarketOrder(units string, instrument string, stopLossPrice string,
 	takeProfitPrice string) oanda.OrdersRequest {
@@ -77,24 +127,6 @@ func MarketOrder(units string, instrument string, stopLossPrice string,
 	//take profit data
 	takeProfitOnFill := oanda.TakeProfitOnFill{
 		TimeInForce: "GTC", Price: takeProfitPrice,
-	}
-
-	//submiting order with no takeProfit or stoploss
-	if stopLossPrice == "" {
-		//order data
-		orders := oanda.SimpleOrdersRequest{
-			Orders: oanda.Orders{
-				TakeProfitOnFill: takeProfitOnFill,
-				TimeInForce:  "FOK",
-				Instrument:   instrument,
-				Units:        units,
-				Type:         "MARKET",
-				PositionFill: "DEFAULT"},
-		}
-
-		fmt.Println("test me - order with no stop loss")
-		return orders
-
 	}
 
 	//order data
