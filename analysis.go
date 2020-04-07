923f0c512c7e73533d09caf5a7fd72c08aabf8ef
nepdave <nepdave1@gmail.com>
2020-04-07 09:33:31 -0700

    added true range func

--- analysis.go
+++ analysis.go
@@ -2,6 +2,7 @@ package fxtechnical
 
 import (
 	"fmt"
+	"math"
 	oanda "github.com/nepdave/oanda"
 	"strconv"
 )
@@ -24,18 +25,72 @@ func CloseAverage(i *oanda.InstrumentHistory) (float64, error) {
 	return average, nil
 }
 
-//AverageTrueRange return the AverageTrueRange until 
-//func AverageTrueRange(i *oanda.InstrumentHistory) (float64, error) {
+//TrueRange returns the AverageTrueRange, defined as TR=Max[(H−L),Abs(H−CP),Abs(L−CP)]
+func TrueRange(midCurrent oanda.Mid, midPrevious oanda.Mid) (float64, error) {
+		ranges := []float64{}
+
+		//getting the H - L
+		currentHigh, err := strconv.ParseFloat(midCurrent.H, 64)
+		if err != nil {
+			return 0, err
+		}
+		currentLow, err := strconv.ParseFloat(midPrevious.L, 64)
+		if err != nil {
+			return 0, err
+		}
+		ranges = append(ranges, currentHigh-currentLow)
+
+		//getting the H - CP
+		closePrevious, err := strconv.ParseFloat(midPrevious.C, 64)
+		if err != nil {
+			return 0, err
+		}
+		ranges = append(ranges, math.Abs(currentHigh - closePrevious))
+
+		//getting the L - CP
+		ranges = append(ranges, math.Abs(currentLow - closePrevious))
+
+		//getting the max range ie, the true range
+		trueRange := 0.0
+		for _,v := range ranges {
+			if v > trueRange {
+				trueRange = v
+			}
+		}
+		fmt.Printf("TRUE RANGE: %f\n", trueRange)
+		return trueRange, nil
+}
 
 //sigma notation in golang format
-func AverageTrueRange() {
-	fmt.Println("Hello, playground")
+func AverageTrueRange(iH *oanda.InstrumentHistory) (float64, error) {
+//	sum := 0
+//	for i := 0; i <= 3; i++ {
+//		sum = sum + i
+//		fmt.Printf("i: %d sum: %d\n", i, sum)
+//	}
 
-	sum := 0
-	for i := 0; i <= 3; i++ {
-		sum = sum + i
-		fmt.Printf("i: %d sum: %d\n", i, sum)
+//	var sum float64 = 0.0
+	//for i, v := range i.Candles {
+	fmt.Println("LEN OF I")
+	fmt.Println(len(iH.Candles))
+//	for j := 0; j <len(iH.Candles); j++ {
+//		
+//	}
+	trueRangeSum := 0.0
+	//for i,_ := range len(iH.Candles) {
+	for i := len(iH.Candles) - 1; i >= 1; i-- {
+		trueRange, err := TrueRange(iH.Candles[i].Mid, iH.Candles[i-1].Mid)
+		if err != nil {
+			return 0, err
+		}
+
+		trueRangeSum = trueRangeSum + trueRange
+
 	}
+
+	fmt.Printf("TRUE RANGE SUM: %f\n", trueRangeSum)
+
+	return 0, nil
 }
 
 /*
@@ -50,7 +105,5 @@ ATR=(1/n)*(i=1 N over SIGMA for TR)
 ATR=(1/n) (n)(i=1)∑TR i
 ​
 */
-	
 
 
-}
