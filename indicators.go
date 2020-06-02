7c00af8373bd6eb6a3d45559092e941e485eab6f
nepdave <nepdave1@gmail.com>
2020-06-01 20:29:30 -0700

    small change

--- indicators.go
+++ indicators.go
@@ -97,6 +97,29 @@ func VolumeAverage(insHist *oanda.InstrumentHistory) (float64, error) {
 	return average, nil
 }
 
+//ATR=(1/n)*(i=1 N over SIGMA for TR) or ATR=(1/n) (n)(i=1)∑TR i
+func AverageTrueRange(insHist *oanda.InstrumentHistory) (float64, error) {
+
+	n := len(insHist.Candles)
+	trueRangeSum := 0.0
+
+	for i := len(insHist.Candles) - 2; i >= 1; i-- {
+		trueRange, err := TrueRange(insHist.Candles[i].Mid, insHist.Candles[i-1].Mid)
+		if err != nil {
+			return 0, err
+		}
+		trueRangeSum = trueRangeSum + trueRange
+	}
+
+	atrString := fmt.Sprintf("%.4f", (1.0/float64(n))*trueRangeSum)
+	averageTrueRange, err := strconv.ParseFloat(atrString, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return averageTrueRange, nil
+}
+
 //TrueRange returns the True Range, defined as TR=Max[(H−L),Abs(H−CP),Abs(L−CP)]
 func TrueRange(midCurrent oanda.Mid, midPrevious oanda.Mid) (float64, error) {
 	ranges := []float64{}
@@ -131,27 +154,4 @@ func TrueRange(midCurrent oanda.Mid, midPrevious oanda.Mid) (float64, error) {
 	}
 
 	return trueRange, nil
-}
-
-//ATR=(1/n)*(i=1 N over SIGMA for TR) or ATR=(1/n) (n)(i=1)∑TR i
-func AverageTrueRange(insHist *oanda.InstrumentHistory) (float64, error) {
-
-	n := len(insHist.Candles)
-	trueRangeSum := 0.0
-
-	for i := len(insHist.Candles) - 2; i >= 1; i-- {
-		trueRange, err := TrueRange(insHist.Candles[i].Mid, insHist.Candles[i-1].Mid)
-		if err != nil {
-			return 0, err
-		}
-		trueRangeSum = trueRangeSum + trueRange
-	}
-
-	atrString := fmt.Sprintf("%.4f", (1.0/float64(n))*trueRangeSum)
-	averageTrueRange, err := strconv.ParseFloat(atrString, 64)
-	if err != nil {
-		return 0, err
-	}
-
-	return averageTrueRange, nil
 }
