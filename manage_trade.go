30e02ec93c0e13bfd1030dc38bde1e74f52be878
nepdave <nepdave1@gmail.com>
2020-12-31 08:48:23 -0800

    new func

--- manage_trade.go
+++ manage_trade.go
@@ -1,5 +1,11 @@
 package fxtechnical
 
+import (
+	"fmt"
+	"github.com/nepdave/oanda"
+	"strconv"
+)
+
 type TradeTargets struct {
 	TargetOne       string
 	TargetOneStatus string
@@ -8,4 +14,15 @@ type TradeTargets struct {
 
 func ManageTrade(tradeID string) (string, error) {
 	return "OPEN", nil
+}
+
+func GetHalfOfCurrentUnits(trade *oanda.Trade) (string, error) {
+	currentUnits, err := strconv.ParseFloat(trade.CurrentUnits, 64)
+
+	if err != nil {
+		return "", err
+	}
+
+	unitsToReduceFloat := currentUnits / 2
+	return fmt.Sprint(int(unitsToReduceFloat)), nil
 }
--- risk.go
+++ risk.go
@@ -3,12 +3,14 @@ package fxtechnical
 import (
 	"fmt"
 	oanda "github.com/nepdave/oanda"
+	"github.com/nepdave/xe"
 	"strconv"
+	"strings"
 )
 
 //CalculateRiskCapitol takes a margin available percent you want to risk
 //on a trade and converts the percentage to a number of units to risk
-func CalculateRiskCapitol(marginPercent float64) (string, error) {
+func CalculateRiskCapitol(marginToRisk float64) (string, error) {
 	//get account summary
 	accountSummary, err := oanda.GetAccountSummary()
 
@@ -23,10 +25,10 @@ func CalculateRiskCapitol(marginPercent float64) (string, error) {
 		return "", err
 	}
 
-	//setting the riskCapitol to the marginPercent passed in
-	//typically the marginPercent should be about 2% or 3%
+	//setting the riskCapitol to the marginToRisk passed in
+	//typically the marginToRisk should be about 2% or 3%
 	//of the margin available
-	riskCapitolFloat := marginAvailable * marginPercent
+	riskCapitolFloat := marginAvailable * marginToRisk
 
 	//multiplying the risk capitol by 20 to account for leverage
 	//currently using 20:1 leverage
@@ -34,3 +36,56 @@ func CalculateRiskCapitol(marginPercent float64) (string, error) {
 
 	return fmt.Sprintf("%d", int(riskCapitolFloat)), nil
 }
+
+//UnitsToRisk takes a margin available percent you want to risk
+//on a trade and converts the percentage to a number of units to risk
+func UnitsToRisk(marginToRisk float64, marginRatio float64, instrument string) (string, error) {
+	//get account summary
+	accountSummary, err := oanda.GetAccountSummary()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//get the margin available from the account summary
+	marginAvailable, err := strconv.ParseFloat(accountSummary.Account.MarginAvailable, 64)
+
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("margin available: %v\n", marginAvailable)
+
+	//convert home currency to base currency
+	currencies := strings.Split(instrument, "_")
+
+	exchangeRate, err := xe.GetExchangeRate(currencies[0], currencies[1])
+
+	fmt.Printf("exchange rate: %v\n", exchangeRate.To[0].Mid)
+
+	if err != nil {
+		return "", err
+	}
+
+	unitsToRisk := ((marginAvailable * marginToRisk) * marginRatio) / exchangeRate.To[0].Mid
+
+	return fmt.Sprintf("%d", int(unitsToRisk)), nil
+}
+
+/*
+How This Tool Works
+This calculation uses the following formula:
+
+Margin Available * (margin ratio) / ({BASE}/{HOME Currency} Exchange Rate)
+For example, suppose:
+
+Home Currency: USD
+Currency Pair: GBP/CHF
+Margin Available: 100
+Margin Ratio : 20:1
+Base / Home Currency: GBP/USD = 1.584
+Then,
+
+Units = (100 * 20) / 1.584
+Units = 1262
+*/
